ffe125f395bcf462e4a5b9d4f952f5c73190cbaa
Igor <igormax2008@gmail.com>
2023-01-22 19:23:45 -0300

    test: :white_check_mark: Corrigindo testes para assegurar 100% no code coverage

--- math.go
+++ math.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(soma(69, 200))
-	fmt.Println(subtracao2(69, 200))
-	fmt.Println(multiplicacao(69, 200))
-	fmt.Println(divisao(69, 200))
-	fmt.Println(soma2(69, 200))
-	fmt.Println(subtracao2(69, 200))
-	fmt.Println(multiplicacao2(69, 200))
-	fmt.Println(divisao2(69, 200))
+
 }
 
 func soma(a int, b int) int {
@@ -26,21 +17,5 @@ func multiplicacao(a int, b int) int {
 }
 
 func divisao(a int, b int) int {
-	return a / b
-}
-
-func soma2(a int, b int) int {
-	return a + b
-}
-
-func subtracao2(a int, b int) int {
-	return a - b
-}
-
-func multiplicacao2(a int, b int) int {
-	return a * b
-}
-
-func divisao2(a int, b int) int {
 	return a / b
 }
