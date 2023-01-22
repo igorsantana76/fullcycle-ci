6dc11ef3fa4b80b5ef44d6548dfe1edd02cd174c
Igor <igormax2008@gmail.com>
2023-01-22 18:56:54 -0300

    ci: :sparkles: CI com verificação do Sonarcloud

--- src/math.go
+++ src/math.go
@@ -1,11 +1,46 @@
-package main
+package foo
 
 import "fmt"
 
 func main() {
 	fmt.Println(soma(69, 200))
+	fmt.Println(subitracao(69, 200))
+	fmt.Println(multiplicacao(69, 200))
+	fmt.Println(divisao(69, 200))
+	fmt.Println(soma2(69, 200))
+	fmt.Println(subitracao2(69, 200))
+	fmt.Println(multiplicacao2(69, 200))
+	fmt.Println(divisao2(69, 200))
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func subitracao(a int, b int) int {
+	return a - b
+}
+
+func multiplicacao(a int, b int) int {
+	return a * b
+}
+
+func divisao(a int, b int) int {
+	return a / b
+}
+
+func soma2(a int, b int) int {
+	return a + b
+}
+
+func subitracao2(a int, b int) int {
+	return a - b
+}
+
+func multiplicacao2(a int, b int) int {
+	return a * b
+}
+
+func divisao2(a int, b int) int {
+	return a / b
 }
--- src/math_test.go
+++ src/math_test.go
@@ -1,12 +1,39 @@
-package main
+package foo
 
 import "testing"
 
-func TestSoma(t *testing.T) {
+func TestSomar(t *testing.T) {
 
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
+		t.Error("Resultado deve ser 30")
+	}
+}
+
+func TestSubtrair(t *testing.T) {
+
+	total := subitracao(15, 15)
+
+	if total != 0 {
+		t.Error("Resultado deve ser 0")
+	}
+}
+
+func TestMultiplicar(t *testing.T) {
+
+	total := multiplicacao(15, 15)
+
+	if total != 225 {
+		t.Error("Resultado deve ser 225")
+	}
+}
+
+func TestDividir(t *testing.T) {
+
+	total := divisao(15, 15)
+
+	if total != 1 {
+		t.Error("Resultado deve ser 1")
 	}
 }
